f5430ac2a5c66d9bb7219631c1545cd176698649
voiys <matej2714@gmail.com>
2021-02-25 22:27:26 +0100

    tested both input fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import { Box, ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { FC, useState } from 'react';
 import Input from './components/Input';
 import { OffsetDate } from './utils/OffsetDate';
 import { ScheduleDate } from './utils/ScheduleDate';
 
 const App: FC = () => {
-  const tomorrow = new ScheduleDate(new OffsetDate());
-  const [value, setValue] = useState(tomorrow.shiftStart[0].toString());
+  const tomorrow = new ScheduleDate(new OffsetDate(1));
+  const [hours, setHours] = useState(tomorrow.shiftStart[0].toString());
+  const [minutes, setMinutes] = useState(tomorrow.shiftStart[1].toString());
 
   return (
     <ChakraProvider resetCSS>
-      <Input date={tomorrow} value={value} setValue={setValue} type='hour' />
+      <Input date={tomorrow} value={hours} setValue={setHours} type='hour' />
+      <Input
+        date={tomorrow}
+        value={minutes}
+        setValue={setMinutes}
+        type='minute'
+      />
     </ChakraProvider>
   );
 };
