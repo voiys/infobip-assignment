26195fefc811de049c7a86435d68b09c897f5103
voiys <matej2714@gmail.com>
2021-02-25 22:13:30 +0100

    created input field component and tested it out a bit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Box, ChakraProvider } from '@chakra-ui/react';
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import Input from './components/Input';
+import { OffsetDate } from './utils/OffsetDate';
+import { ScheduleDate } from './utils/ScheduleDate';
 
 const App: FC = () => {
+  const tomorrow = new ScheduleDate(new OffsetDate());
+  const [value, setValue] = useState(tomorrow.shiftStart[0].toString());
+
   return (
     <ChakraProvider resetCSS>
-      <Box>hi</Box>
+      <Input date={tomorrow} value={value} setValue={setValue} type='hour' />
     </ChakraProvider>
   );
 };
