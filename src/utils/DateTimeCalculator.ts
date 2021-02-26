795224f6bc0f5e7a0761dbbb7a5255f169105790
voiys <matej2714@gmail.com>
2021-02-26 02:41:46 +0100

    created a more understandable way of calculating appointments via their minute factor

--- src/utils/DateTimeCalculator.ts
+++ src/utils/DateTimeCalculator.ts
@@ -1,11 +1,61 @@
-import { Time } from '../types/Time';
+import { ScheduleDate } from './ScheduleDate';
 
 class DateTimeCalculator {
-  static addTimes(time1: Time, time2: Time): Time {
-    const [hours1, minutes1] = time1;
-    const [hours2, minutes2] = time2;
+  static isDuringAppointment(
+    appointment: ScheduleDate,
+    targetAppointment: ScheduleDate
+  ) {
+    const { minuteFactor: appointmentFactor } = appointment;
+    const { minuteFactor: targetAppointmentFactor } = targetAppointment;
+    const {
+      minuteFactor: targetWithDurationFactor,
+    } = DateTimeCalculator.addAppointmentDuration(targetAppointment);
 
-    return [hours1 + hours2, minutes1 + minutes2];
+    // @todo - test this
+    const isBetween =
+      appointmentFactor >= targetAppointmentFactor &&
+      appointmentFactor < targetWithDurationFactor;
+
+    return isBetween;
+  }
+
+  static isBeforeAppointment(
+    appointment: ScheduleDate,
+    targetAppointment: ScheduleDate
+  ) {
+    const { minuteFactor: appointmentFactor } = appointment;
+    const {
+      minuteFactor: targetAppointmentFactor,
+    } = DateTimeCalculator.subtractAppointmentDuration(targetAppointment);
+
+    // @todo - test this
+    const isBefore = appointmentFactor < targetAppointmentFactor;
+
+    return isBefore;
+  }
+
+  static addAppointmentDuration(date: ScheduleDate) {
+    const newDate = date.createAppointment();
+    const [hours, minutes] = newDate.time;
+    const [
+      appointmentHours,
+      appointmentMinutes,
+    ] = newDate.config.appointmentDuration;
+    newDate.setHours(hours + appointmentHours, minutes + appointmentMinutes);
+
+    return newDate;
+  }
+
+  static subtractAppointmentDuration(date: ScheduleDate) {
+    const newDate = date.createAppointment();
+    const [hours, minutes] = newDate.time;
+    const [
+      appointmentHours,
+      appointmentMinutes,
+    ] = newDate.config.appointmentDuration;
+    newDate.setHours(hours - appointmentHours, minutes - appointmentMinutes);
+
+    return newDate;
   }
 }
 
